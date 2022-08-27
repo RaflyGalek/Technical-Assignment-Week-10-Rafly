a628783ed3a7f20aefd2665bf946da02a354b3c5
Rafly_Rizqullah <104709795+RaflyGalek@users.noreply.github.com>
2022-08-27 14:21:46 +0700

    Create ultra.py

---
+++ ultra.py
@@ -0,0 +1,111 @@
+import time
+import requests
+import os
+import RPi.GPIO as GPIO
+
+TOKEN = "BBFF-fOBpYuxMljGnFrL2x5FiyuUMA4ewon"  # Put your TOKEN here
+DEVICE_LABEL = "aquathings"  # Put your device label here 
+VARIABLE_LABEL_1 =  "SUHU"# Put your first variable label here
+VARIABLE_LABEL_2 = "JARAK"  # Put your second variable label here
+#VARIABLE_LABEL_3 = "position"  # Put your second variable label here
+
+GPIO.setmode(GPIO.BCM)
+ 
+GPIO_TRIGGER = 17
+GPIO_ECHO = 27
+
+GPIO.setwarnings(False)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+    
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+
+    
+def sensor():
+    for i in os.listdir('/sys/bus/w1/devices'):
+        if i != 'w1_bus_master1':
+            ds18b20 = i
+    return ds18b20
+
+def read(ds18b20):
+    location = '/sys/bus/w1/devices/' + ds18b20 + '/w1_slave'
+    tfile = open(location)
+    text = tfile.read()
+    tfile.close()
+    secondline = text.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature = float(temperaturedata[2:])
+    celsius = temperature / 1000
+    #farenheit = (celsius * 1.8) + 32
+    return celsius
+                  
+def build_payload(variable_1,variable_2): 
+    value_1 = read("28-3c01f096ee94")
+    value_2 = distance()
+    payload = {variable_1: value_1,
+               variable_2: value_2}
+    return payload
+
+
+def post_request(payload):
+    # Creates the headers for the HTTP requests
+    url = "http://industrial.api.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+
+    # Makes the HTTP requests
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+
+    # Processes results
+    print(req.status_code, req.json())
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+
+    print("[INFO] request made properly, your device is updated")
+    return True
+
+
+def main():
+    payload = build_payload(VARIABLE_LABEL_1,VARIABLE_LABEL_2)
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
+
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
